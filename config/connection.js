7cd23fb699eb43e2bc0ed4e8bf35675cade275a3
Manasi <manudee@gmail.com>
2017-12-03 12:40:41 -0500

    Added connection files for burger

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var connection - mysql.createConnection({
+
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: '123456',
+	database: 'burgers_db'
+
+});
+
+//make connection to db
+connection.connect(function(err){
+	if(err)
+		{
+			console.error("Error connecting " + err.stack);
+				return;
+			}
+
+			console.log("Connected as id " + connection.threadId);
+});
+
+
+//export connection so it can be used in orm
+module.exports = connection;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
-
+// app.use('/img', express.static(__dirname + '/public/assets'));
 
 
 app.engine("handlebars", exphbs({ defaulLayout: "main"}));
@@ -16,6 +16,12 @@ app.set("view engine","handlebars");
 var routes = require('./controllers/burgers_controller.js');
 
 app.use('/',routes);
-app.listen(PORT);
+// app.use(app.router);
+// routes.initialize(app);
+
+
+app.listen(PORT,function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
 
 
