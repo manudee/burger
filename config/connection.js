ed15a1235e3338241c184c8f2e207dbfa72ece51
Manasi <manudee@gmail.com>
2017-12-06 22:42:57 -0500

    Updated jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,33 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection;
 
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '123456',
-	database: 'burgers_db'
 
-});
+if(process.env.JAWSDB_URL){
+
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+	connection = mysql.createConnection({
+
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '123456',
+		database: 'burgers_db'
+
+	});
+}
 
 //make connection to db
 connection.connect(function(err){
 	if(err)
-		{
-			console.error("Error connecting " + err.stack);
-				return;
-			}
+	{
+		console.error("Error connecting " + err.stack);
+		return;
+	}
 
-			console.log("Connected as id " + connection.threadId);
+	console.log("Connected as id " + connection.threadId);
 });
 
 
